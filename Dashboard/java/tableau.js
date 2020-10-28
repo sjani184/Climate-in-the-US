ed3e066357ed2d905c5969598c67c4768f0d908c
safieoscar <65041538+safieoscar@users.noreply.github.com>
2020-10-28 16:36:14 -0600

    final 2

--- Dashboard/java/tableau.js
+++
@@ -1,9 +0,0 @@
-function initViz() {
-    url = "https://public.tableau.com/views/ev_sales_trial/Sheet1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link",
-    options = {
-        hideToolbar: true,
-        width: "100%",
-        height: "200px",
-    };
-    viz = new tableau.Viz(tabSheet1, url, options);
-  }
